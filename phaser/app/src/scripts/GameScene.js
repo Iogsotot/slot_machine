8e2830c3d60ee17a62c764127e567c7876daf4f8
Iogsotot <ajustusa@gmail.com>
2021-02-17 15:42:19 +0300

    feat: add smooth animation

--- phaser/app/src/scripts/GameScene.js
+++ phaser/app/src/scripts/GameScene.js
@@ -5,15 +5,49 @@ export default class GameScene extends Phaser.Scene {
     super({ key: 'GameScene' });
   }
 
-  create() {
-    this.spin = [];
-    console.log('create game scene');
-    this.cameras.main.setBackgroundColor('rgb(34,139,34)');
+  startAnims(target, frameRate) {
+    this.target = target;
+    this.frameRate = frameRate;
+    this.tweens.add({
+      targets: this.target.anims,
+      timeScale: { from: 0.2, to: 3 },
+      duration: 4000,
+    });
 
-    this.slider1 = this.add.sprite(138, 0, 'icons', 0);
-    this.slider2 = this.add.sprite(265, 0, 'icons', 0);
-    this.slider3 = this.add.sprite(392, 0, 'icons', 0);
-    
+    this.target.play({ key: 'spin', frameRate: this.frameRate });
+  }
+
+  stopAnims(target) {
+    this.currentTimeScale = target.anims.timeScale;
+    this.tweens.add({
+      targets: target.anims,
+      timeScale: { from: this.currentTimeScale, to: 0 },
+      duration: 4000,
+      onComplete: () => { this.checkFrame(target.anims.currentFrame.index, target); },
+    });
+  }
+
+  checkFrame(frame, target) {
+    this.frame = frame;
+    if (this.frame % 4 === 0) {
+      target.anims.nextFrame();
+    } else if (this.frame % 4 === 2) {
+      target.anims.nextFrame();
+      setTimeout(() => {
+        target.anims.nextFrame();
+      }, 270, this);
+      setTimeout(() => {
+        target.anims.nextFrame();
+      }, 540, this);
+    } else if (this.frame % 4 === 3) {
+      target.anims.nextFrame();
+      setTimeout(() => {
+        target.anims.nextFrame();
+      }, 270, this);
+    }
+  }
+
+  addAnims() {
     this.anims.create({
       key: 'spin',
       frames: this.anims.generateFrameNames('icons', {
@@ -24,109 +58,93 @@ export default class GameScene extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
-    
+  }
+
+  create() {
+    this.spin = [];
+    console.log('create game scene');
+    this.cameras.main.setBackgroundColor('rgb(34,139,34)');
+    this.addAnims();
+
+    this.slider1 = this.add.sprite(138, 0, 'icons', 0);
+    this.slider2 = this.add.sprite(265, 0, 'icons', 0);
+    this.slider3 = this.add.sprite(392, 0, 'icons', 0);
+
     this.input.keyboard.on('keydown-SPACE', () => {
-      this.slider1.play({ key: 'spin' });
-      this.slider2.play({ key: 'spin', frameRate: '24' });
-      this.slider3.play({ key: 'spin' });
+      this.startAnims(this.slider1, 15);
+      this.startAnims(this.slider2, 10);
+      this.startAnims(this.slider3, 20);
     }, this);
-    
+
     this.input.keyboard.on('keydown-P', () => {
       if (this.slider1.anims.isPaused || this.slider2.anims.isPaused || this.slider3.anims.isPaused) {
         this.slider1.anims.resume();
         this.slider2.anims.resume();
         this.slider3.anims.resume();
       } else {
-        this.slider1.anims.stop();
-        this.slider2.anims.stop();
-        this.slider3.anims.stop();
-        const frame1 = this.slider1.anims.currentFrame.index;
-        const frame2 = this.slider2.anims.currentFrame.index;
-        const frame3 = this.slider3.anims.currentFrame.index;
+        // console.log(this.slider1.anims.timeScale);
+        // this.slider1.anims.stop();
+        // this.slider2.anims.stop();
+        // this.slider3.anims.stop();
+        this.stopAnims(this.slider1);
+        this.stopAnims(this.slider2);
+        this.stopAnims(this.slider3);
 
-        if (frame1 % 4 === 0) {
-          console.log(`s1e0: ${frame1}`);
-          this.slider1.anims.nextFrame();
-        } else if (frame1 % 4 === 2) {
-          console.log(`s1e2: ${frame1}`);
-          this.slider1.anims.nextFrame();
-          this.slider1.anims.nextFrame();
-          this.slider1.anims.nextFrame();
-          console.log(this.slider1.anims.currentFrame.index);
-        } else if (frame1 % 4 === 3) {
-          console.log(`s1e3: ${frame1}`);
-          this.slider1.anims.nextFrame();
-          this.slider1.anims.nextFrame();
-          console.log(this.slider1.anims.currentFrame.index);
-        }
-        console.log(`s1 any: ${frame1}`);
+        // const frame1 = this.slider1.anims.currentFrame.index;
+        // const frame2 = this.slider2.anims.currentFrame.index;
+        // const frame3 = this.slider3.anims.currentFrame.index;
 
-        if (frame2 % 4 === 0) {
-          this.slider2.anims.nextFrame();
-        } else if (frame2 % 4 === 2) {
-          this.slider2.anims.nextFrame();
-          this.slider2.anims.nextFrame();
-          this.slider2.anims.nextFrame();
-        } else if (frame2 % 4 === 3) {
-          this.slider2.anims.nextFrame();
-          this.slider2.anims.nextFrame();
-        }
+        // if (frame1 % 4 === 0) {
+        //   this.slider1.anims.nextFrame();
+        // } else if (frame1 % 4 === 2) {
+        //   this.slider1.anims.nextFrame();
+        //   this.slider1.anims.nextFrame();
+        //   this.slider1.anims.nextFrame();
+        // } else if (frame1 % 4 === 3) {
+        //   this.slider1.anims.nextFrame();
+        //   this.slider1.anims.nextFrame();
+        // }
 
-        if (frame3 % 4 === 0) {
-          this.slider3.anims.nextFrame();
-        } else if (frame3 % 4 === 2) {
-          this.slider3.anims.nextFrame();
-          this.slider3.anims.nextFrame();
-          this.slider3.anims.nextFrame();
-        } else if (frame3 % 4 === 3) {
-          this.slider3.anims.nextFrame();
-          this.slider3.anims.nextFrame();
-        }
+        // if (frame2 % 4 === 0) {
+        //   this.slider2.anims.nextFrame();
+        // } else if (frame2 % 4 === 2) {
+        //   this.slider2.anims.nextFrame();
+        //   this.slider2.anims.nextFrame();
+        //   this.slider2.anims.nextFrame();
+        // } else if (frame2 % 4 === 3) {
+        //   this.slider2.anims.nextFrame();
+        //   this.slider2.anims.nextFrame();
+        // }
+
+        // if (frame3 % 4 === 0) {
+        //   this.slider3.anims.nextFrame();
+        // } else if (frame3 % 4 === 2) {
+        //   this.slider3.anims.nextFrame();
+        //   this.slider3.anims.nextFrame();
+        //   this.slider3.anims.nextFrame();
+        // } else if (frame3 % 4 === 3) {
+        //   this.slider3.anims.nextFrame();
+        //   this.slider3.anims.nextFrame();
+        // }
       }
     }, this);
 
     // test next frame
     this.input.keyboard.on('keydown-N', () => {
-      // const frame1 = this.slider1.anims.currentFrame.index;
-      // console.log(`1${frame1}`);
-      console.log(this.slider1.anims.currentFrame.index);
       this.slider1.anims.nextFrame();
-      console.log(this.slider1.anims.currentFrame.index);
     });
 
     // ускорение и замедление
-    this.tweens.add({
-      targets: this.slider1.anims,
-      timeScale: { from: 0.8, to: 2 },
-      ease: 'Sine.inOut',
-      // yoyo: true,
-      repeat: -1,
-      repeatDelay: 1000,
-      hold: 1000,
-      duraton: 3000,
-    });
-
-    this.tweens.add({
-      targets: this.slider2.anims,
-      timeScale: { from: 0.8, to: 2 },
-      ease: 'Sine.inOut',
-      // yoyo: true,
-      repeat: -1,
-      repeatDelay: 1000,
-      hold: 1000,
-      duraton: 3000,
-    });
-
-    this.tweens.add({
-      targets: this.slider3.anims,
-      timeScale: { from: 0.8, to: 2 },
-      ease: 'Sine.inOut',
-      // yoyo: true,
-      repeat: -1,
-      repeatDelay: 1000,
-      hold: 1000,
-      duraton: 3000,
-    });
+    // this.tweens.add({
+    //   targets: this.slider1.anims,
+    //   timeScale: { from: 0.5, to: 2 },
+    //   ease: 'Sine.inOut',
+    //   // yoyo: true,
+    //   repeat: -1,
+    //   repeatDelay: 1000,
+    //   // hold: 1000,
+    //   duraton: 2000,
+    // });
 
     this.window = this.add.sprite(0, 60, 'window').setOrigin(0, 0).setScale(0.35).setAlpha(0.3);
   }
